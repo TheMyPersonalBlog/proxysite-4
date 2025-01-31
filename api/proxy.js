2696e2a19f3d71c12cdfbb52b829b40c2487a7f1
NathanBrodrick <163065342+Iambrucea@users.noreply.github.com>
2025-01-31 15:10:42 -0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://ny.browserfps.com/";//your website url
+  let target = "https://lordz.io/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
